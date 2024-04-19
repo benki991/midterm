d462e94069dceb15f4c467b125ab0110130e4cc8
Kevin Payos <kevinbarret.payos@gmail.com>
2024-04-19 22:02:12 +0800

    Modal info

--- midtermApp/views.py
+++ midtermApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
+
+def home(req):
+    return HttpResponse(render(req, "index.html", context={"id":["Jemwel", "Stephen", "Joswa", "Kevin"]}))
