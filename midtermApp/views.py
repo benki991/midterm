c44dde9639fd0b25b4f630ce233cd069662e56f7
Kevin Payos <kevinbarret.payos@gmail.com>
2024-04-19 22:44:27 +0800

    Custom data on array

--- midtermApp/views.py
+++ midtermApp/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def home(req):
-    return HttpResponse(render(req, "index.html", context={"id":["Jemwel", "Stephen", "Joswa", "Kevin"]}))
+    context = {
+        "id":[
+            {"name": "Jemwel", "data": "Jose Rizal"},
+            {"name": "Joswa", "data": "shao long bao"},
+            {"name": "Stephen", "data": "spidigong"},
+            {"name": "Kevin", "data": "im partially black"},
+        ], 
+    }
+    return HttpResponse(render(req, "index.html", context))
