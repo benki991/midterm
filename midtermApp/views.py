9d340507f32d9270d8842e9f47d99c53c5990dec
Kevin Payos <kevinbarret.payos@gmail.com>
2024-04-21 20:20:30 +0800

    Profile page with data done

--- midtermApp/views.py
+++ midtermApp/views.py
@@ -6,7 +6,7 @@ def home(req):
     context = {
         "id":[
             {"id": 1, "name": "Jemwel", "data": "Jose Rizal"},
-            {"id": 2, "name": "Joswa", "data": "shao long bao"},
+            {"id": 2, "name": "Joshua", "data": "shao long bao"},
             {"id": 3, "name": "Stephen", "data": "spidigong"},
             {"id": 4, "name": "Kevin", "data": "im partially black"},
         ], 
@@ -17,28 +17,32 @@ def profile(req, id):
     wait = False
     match id:
         case '1':
-            name = "Jemwel"
+            name = "Jemwel Jerald Salamida"
             age = 21
-            email = "jemwel@gmail.com"
-            hobbies = {"Basketball", "Linux"}
+            email = "jemsadimalas@gmail.com"
+            hobbies = {"Family Vehicle Operator", "Playing Basketball", "Making Music Snippets"}
+            img = "jemwel"
             wait = True
         case '2':
-            name = "Joswa"
+            name = "Joshua Uy"
             age = 100
-            email = "shab-dealer@pdea.com"
-            hobbies = {"Shab-"}
+            email = "joshualex1234@gmail.com"
+            hobbies = {"Sleeping", "Playing Video Games", "Playing Kalimba"}
+            img = "joshua"
             wait = True
         case '3':
-            name = "Stephen"
-            age = 69
-            email = "spidermansavecity@gmail.com"
-            hobbies = {"Spider"}
+            name = "Stephen Morado"
+            age = 21
+            email = "stepenmorado27@gmail.com"
+            hobbies = {"Watching Anime/Movies", "Playing Video Games", "Playing Guitar"}
+            img = "stephen"
             wait = True
         case '4':
-            name = "Kevin"
+            name = "Kevin Barret Payos"
             age = 21
             email = "kevinbarret.payos@gmail.com"
-            hobbies = {"Sleep"}
+            hobbies = {"Gym", "Programming", "Playing Video Games"}
+            img = "kevin"
             wait = True
         case _:
             wait = False
@@ -50,6 +54,7 @@ def profile(req, id):
             "age": age,
             "email": email,
             "hobbies": hobbies,
+            "img": img,
         },
     }     
     if wait == True:
